bb97eb40785d592f490d895fda0041c88e76d389
sabotior <black_lion@mail.ru>
2019-02-12 11:02:31 +0400

    1

--- migrations/m190209_000144_create_users_table.php
+++ migrations/m190209_000144_create_users_table.php
@@ -12,18 +12,12 @@ class m190209_000144_create_users_table extends Migration
      */
     public function safeUp()
     {
-        $this->createTable('{{%user}}', [
+        $this->createTable('users', [
             'id' => $this->primaryKey(),
-            'username' => $this->string()->notNull()->unique(),
-            'auth_key' => $this->string(32)->notNull(),
-            'password_hash' => $this->string()->notNull(),
-            'password_reset_token' => $this->string()->unique(),
-            'email' => $this->string()->notNull()->unique(),
-
-            'status' => $this->smallInteger()->notNull()->defaultValue(10),
-            'created_at' => $this->integer()->notNull(),
-            'updated_at' => $this->integer()->notNull(),
-        ], $tableOptions);
+            'login' => $this->string(50)->notNull(),
+            'password' => $this->string()->notNull(),
+            'email' => $this->string(),
+        ]);
     }
 
     /**
