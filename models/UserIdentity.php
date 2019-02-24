502648d2beaf12fd78fd52f0124cbc0677ef3487
sabotior <black_lion@mail.ru>
2019-02-24 13:13:27 +0400

    2-2
    настроил авторизацию

--- models/UserIdentity.php
+++ models/UserIdentity.php
@@ -2,7 +2,10 @@
 
 namespace app\models;
 
-class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
+
+use app\models\tables\Users;
+
+class UserIdentity extends \yii\base\BaseObject implements \yii\web\IdentityInterface
 {
     public $id;
     public $username;
@@ -33,7 +36,18 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        /*return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;*/
+        if ($user = Users::findOne($id)) {
+            // var_dump($user);
+            $model = new static([
+                    'id' => $user->id,
+                    'username' => $user->login,
+                    /*'password' => $user->password*/
+                ]
+            );
+            return $model;
+            // var_dump($model); exit;
+        }
     }
 
     /**
@@ -58,15 +72,29 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public static function findByUsername($username)
     {
-       /* foreach (self::$users as $user) {
+        /*
+       foreach (self::$users as $user) {
             if (strcasecmp($user['username'], $username) === 0) {
                 return new static($user);
             }
-        }*/
+        }
+        */
         if ($user = Users::findOne(['login' => $username])) {
+           // var_dump($user);
+            $model = new static([
+                    'id' => $user->id,
+                    'username' => $user->login,
+                    'password' => $user->password
+                ]
+            );
+            return $model;
+           // var_dump($model); exit;
+        }
+
+       /* if ($user = Users::findOne(['login' => $username])) {
             $user->setScenario(Users::SCENARIO_AUTH);
             return new static($user->toArray());
-        }
+        }*/
 
         return null;
     }
@@ -103,6 +131,6 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public function validatePassword($password)
     {
-        return $this->password === $password;
+        return $this->password === md5($password);
     }
 }
